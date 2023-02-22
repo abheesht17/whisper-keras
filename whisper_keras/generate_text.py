fc51d049420e5c2d98e2fbab5d9754188e9f2833
abheesht17 <sharmabhee@gmail.com>
2023-02-23 00:26:32 +0530

    Fixes

--- whisper_keras/generate_text.py
+++ whisper_keras/generate_text.py
@@ -38,8 +38,8 @@ def generate(
     model = load_model(preset)
 
     is_multilingual = tokenizer.is_multilingual
-    language_ids = tf.constant(list(LANGUAGE_TO_ID_MAPPING.values()))
-    suppressed_ids = tf.constant(list(ENGLISH_SUPPRESSED_TOKENS.values()))
+    language_ids = tf.constant(LANGUAGE_TO_ID_MAPPING)
+    suppressed_ids = tf.constant(ENGLISH_SUPPRESSED_TOKENS)
 
     decoder_token_ids = tf.constant([[tokenizer.bos_token_id]])
     if is_multilingual:
