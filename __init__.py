f847a7fb3ccaaabf8a24393360893bb2279ee32a
abheesht17 <sharmabhee@gmail.com>
2023-02-23 15:24:20 +0530

    Edit __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-
-from whisper_keras.generate import generate
-from whisper_keras.load_pretrained_objects import load_model, load_tokenizer
-from whisper_keras.preprocessors.load_audio import load_audio
-from whisper_keras.preprocessors.whisper_audio_feature_extractor import (
-    WhisperAudioFeatureExtractor,
-)
